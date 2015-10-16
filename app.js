65ea9efc677ac313082b7bc70ffcd419fb1e0941
daniel-llach <daniel.llach@gmail.com>
2015-10-15 21:27:12 -0300

    cors fix

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ mongoose.connect('mongodb://' + config.db_user + ':' + config.db_password + '@' 
 app = express();
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Key");
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 app.use(bodyParser.json());
