42c15d25af4fbf50df6af7e46b03c6ba383b0771
daniel-llach <daniel.llach@gmail.com>
2015-10-15 22:42:41 -0300

    cors fix

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app = express();
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, username, password");
   next();
 });
 app.use(bodyParser.json());
